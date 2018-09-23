b38d1f2d97dba28ca79cba025b40fb07bc100450
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-23 14:22:33 +0100

    Moving leaflet map code to mapview

--- client/src/views/country_view.js
+++ client/src/views/country_view.js
@@ -41,17 +41,6 @@ CountryView.prototype.render = function (data) {
   const flag = createAppend("img", '', this.element);
   flag.src = data.flag;
   flag.alt = `The ${data.demonym} flag`;
-
-  const lat = data.latlng[0];
-  const long = data.latlng[1];
-
-  let mymap = L.map('mapid').setView([lat, long], 5);
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoibWljaGFlbHRoZXRhbGwiLCJhIjoiY2ptZXZ4aG1rMHZybjNxbzlxbGdoeHRvcCJ9.mGMQxJwXrnJSDi3QiO_C9w'
-}).addTo(mymap);
 };
 
 
--- client/src/views/map_view.js
+++ client/src/views/map_view.js
@@ -1,1 +1,14 @@
 const PubSub = require('../helpers/pub_sub.js');
+
+
+
+const lat = data.latlng[0];
+const long = data.latlng[1];
+
+let mymap = L.map('mapid').setView([lat, long], 5);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1IjoibWljaGFlbHRoZXRhbGwiLCJhIjoiY2ptZXZ4aG1rMHZybjNxbzlxbGdoeHRvcCJ9.mGMQxJwXrnJSDi3QiO_C9w'
+}).addTo(mymap);
