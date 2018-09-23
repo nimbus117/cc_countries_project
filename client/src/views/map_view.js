c31765bd9fd89bef959512429809a2dde59e6a94
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-23 14:24:39 +0100

    Mapview constructor complete

--- client/src/views/map_view.js
+++ client/src/views/map_view.js
@@ -1,14 +1,30 @@
 const PubSub = require('../helpers/pub_sub.js');
 
+const MapView = function(element) {
+  this.element = element;
+}
 
+MapView.prototype.bindEvents = function () {
+  PubSub.subscribe('Countries:country-data', (event) => {
+    const data = event.detail;
+    this.render(data);
+  })
+};
 
-const lat = data.latlng[0];
-const long = data.latlng[1];
+MapView.prototype.render = function (data) {
+  this.element.innerHTML = '';
 
-let mymap = L.map('mapid').setView([lat, long], 5);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.streets',
-  accessToken: 'pk.eyJ1IjoibWljaGFlbHRoZXRhbGwiLCJhIjoiY2ptZXZ4aG1rMHZybjNxbzlxbGdoeHRvcCJ9.mGMQxJwXrnJSDi3QiO_C9w'
-}).addTo(mymap);
+  const lat = data.latlng[0];
+  const long = data.latlng[1];
+
+  let mymap = L.map('mapid').setView([lat, long], 5);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibWljaGFlbHRoZXRhbGwiLCJhIjoiY2ptZXZ4aG1rMHZybjNxbzlxbGdoeHRvcCJ9.mGMQxJwXrnJSDi3QiO_C9w'
+  }).addTo(mymap);
+
+};
+
+module.exports = MapView;
