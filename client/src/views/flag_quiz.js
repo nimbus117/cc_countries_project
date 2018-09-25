0985e9b95f336eaa3f3cad2302d516c5649d46ba
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-25 14:11:02 +0100

    random flag generator complete

--- client/src/views/flag_quiz.js
+++ client/src/views/flag_quiz.js
@@ -26,6 +26,7 @@ FlagQuiz.prototype.start = function () {
   this.currentflags = [];
   this.randomAnswer();
   this.getFlags();
+  this.arrange();
 };
 
 FlagQuiz.prototype.randomAnswer = function () {
@@ -40,11 +41,81 @@ FlagQuiz.prototype.getFlags = function () {
   const rf2 = Math.floor(Math.random()*this.data.length);
   const rf3 = Math.floor(Math.random()*this.data.length);
   this.currentflags.push(rf1,rf2,rf3);
-  console.log(this.currentflags);
+  // console.log(this.currentflags);
 };
 
 FlagQuiz.prototype.arrange = function () {
-  
+  const random1 = this.randomFlag();
+  const random2 = this.randomFlag();
+  const random3 = this.randomFlag();
+  const random4 = this.randomFlag();
+
+  const namediv = createAppend('div','',this.element);
+  const flagdiv1 = createAppend('div','',this.element);
+  const flagdiv2 = createAppend('div','',this.element);
+  const flagdiv3 = createAppend('div','',this.element);
+  const flagdiv4 = createAppend('div','',this.element);
+
+  namediv.id = 'fqname';
+  flagdiv1.id = 'fqflag1';
+  flagdiv2.id = 'fqflag2';
+  flagdiv3.id = 'fqflag3';
+  flagdiv4.id = 'fqflag4';
+
+  const answername = createAppend('h1', `Find the Flag for ${this.data[this.answer].name}!`, namediv);
+  const flag1 = createAppend('img', '', flagdiv1);
+  const flag2 = createAppend('img', '' , flagdiv2);
+  const flag3 = createAppend('img', '', flagdiv3);
+  const flag4 = createAppend('img', '', flagdiv4);
+
+  flag1.src = this.data[random1].flag;
+  flag2.src = this.data[random2].flag;
+  flag3.src = this.data[random3].flag;
+  flag4.src = this.data[random4].flag;
+
+  flag1.addEventListener('click', e => {
+    if (flag1.src === this.data[this.answer].flag) {
+      this.score++;
+    }
+    this.next();
+    console.log(this.score);
+  });
+  flag2.addEventListener('click', e => {
+    if (flag2.src === this.data[this.answer].flag) {
+      this.score++;
+    }
+    this.next();
+    console.log(this.score);
+  });
+  flag3.addEventListener('click', e => {
+    if (flag3.src === this.data[this.answer].flag) {
+      this.score++;
+    }
+    this.next();
+    console.log(this.score);
+  });
+  flag4.addEventListener('click', e => {
+    if (flag4.src === this.data[this.answer].flag) {
+      this.score++;
+    }
+    this.next();
+    console.log(this.score);
+  });
+};
+
+FlagQuiz.prototype.randomFlag = function () {
+  const result = this.currentflags[Math.floor(Math.random()*this.currentflags.length)];
+  // console.log(result);
+  // console.log(this.currentflags);
+  const index = this.currentflags.indexOf(result);
+  this.currentflags.splice(index, 1);
+  // console.log(this.currentflags);
+  return result;
+};
+
+FlagQuiz.prototype.next = function () {
+  this.turn++;
+console.log(this.turn);
 };
 
 // Sub all country data *
