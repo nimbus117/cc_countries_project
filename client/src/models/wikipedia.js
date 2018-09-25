36dbc143922b9243c9902649a0ab60fbe27730c7
Matt Addison <matthew.addison@talk21.com>
2018-09-25 09:52:39 +0100

    fixed data not refreshing bug

--- client/src/models/wikipedia.js
+++ client/src/models/wikipedia.js
@@ -10,15 +10,19 @@ Wikipedia.prototype.bindEvents = function () {
     countryName = event.detail.name;
     this.getData(countryName);
   });
-  // publish wiki country blurb
 };
 
 Wikipedia.prototype.getData = function (countryName) {
   new Request(`http://localhost:3000/wikipedia/${countryName}`)
   .get()
   .then(data => {
-    console.log(data);
+    this.data = data;
+    const countryData = this.data.query.pages;
+    const countryDataText = Object.values(countryData)[0].extract;
+    PubSub.publish('Wikipedia:country-text', countryDataText);
+    console.log(countryDataText);
   })
+  .catch(console.error)
 };
 
 
