0be9738ed7fe1fba657a87a284a77d3803faa608
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-25 11:56:13 +0100

    flag array generated

--- client/src/views/flag_quiz.js
+++ client/src/views/flag_quiz.js
@@ -19,32 +19,38 @@ FlagQuiz.prototype.bindEvents = function () {
 };
 
 FlagQuiz.prototype.start = function () {
+  this.element.innerHTML = '';
   this.answerlist = [];
   this.score = 0;
   this.turn = 1;
+  this.currentflags = [];
   this.randomAnswer();
-  this.randomFlag();
+  this.getFlags();
 };
 
 FlagQuiz.prototype.randomAnswer = function () {
   this.answer = Math.floor(Math.random()*this.data.length);
   this.answerlist.push(this.answer);
   this.currentflags.push(this.answer);
+  // console.log(this.answer);
 };
 
-FlagQuiz.prototype.randomFlag = function () {
+FlagQuiz.prototype.getFlags = function () {
   const rf1 = Math.floor(Math.random()*this.data.length);
   const rf2 = Math.floor(Math.random()*this.data.length);
   const rf3 = Math.floor(Math.random()*this.data.length);
-  console.log(rf1);
-  console.log(rf2);
-  console.log(rf3);
+  this.currentflags.push(rf1,rf2,rf3);
+  console.log(this.currentflags);
+};
+
+FlagQuiz.prototype.arrange = function () {
+  
 };
 
 // Sub all country data *
 // Function RandomAnswer (selects a random country but not one that is in answerlist. Then adds index to answerlist)*
-// Function RandomFlag (all flags except answer)
-// Function AnswerFlag (matches answer)
+// Function RandomFlag (all flags except answer)*
+// Function AnswerFlag (matches answer)*
 // Function Arrange (creates 5 divs, country name on top and random assortment of flags below)
 
 // Greeting text and start button
