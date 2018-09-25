39c99f1465a25237344493258580313e24bd77dc
nimbus117 <mail@nimbus117.co.uk>
2018-09-25 20:37:47 +0100

    refactored MapSelecteView render function

--- client/src/views/map_select_view.js
+++ client/src/views/map_select_view.js
@@ -31,7 +31,6 @@ MapSelectView.prototype.renderMap = function (data) {
     chart: {
       map: 'custom/world-robinson-highres',
       height: '50%',
-      // borderWidth: 2,
       backgroundColor: '#7FCAFF'
     },
     title: {
@@ -51,22 +50,12 @@ MapSelectView.prototype.renderMap = function (data) {
       joinBy: ['iso-a3', 'code'],
       name: 'Index',
       showInLegend: false,
-      states: {
-        hover: {
-          // color: 'black'
-        },
-        select: {
-          color: '#fff000'
-        }
-      }
     }],
     plotOptions:{
       series:{
         colorByPoint: true,
         colors: ['#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE',
            '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92'],
-        // colors: ['#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce',
-        //     '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'],
         cursor: 'pointer',
         point:{
           events:{
